80058825d0b3ac78e3a629070a2fc1650ad9a129
yuteng <soft_yt@163.com>
2020-02-11 00:43:46 +0800

    添加日志

---
+++ src/main/java/com/functionwall/service/serviceImpl/LogServiceImpl.java
@@ -0,0 +1,49 @@
+package com.functionwall.service.serviceImpl;
+
+import com.functionwall.dao.LogMapper;
+import com.functionwall.exception.FunctionWallErrorCode;
+import com.functionwall.exception.FunctionWallRuntimeException;
+import com.functionwall.pojo.mould.LogDomain;
+import com.functionwall.service.LogService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 请求日志
+ */
+@Service
+public class LogServiceImpl implements LogService {
+
+    @Autowired
+    private LogMapper logDao;
+
+    @Override
+    public void addLog(String action, String data, String ip, Integer authorId) {
+        LogDomain logDomain = new LogDomain();
+        logDomain.setAuthorId(authorId);
+        logDomain.setIp(ip);
+        logDomain.setData(data);
+        logDomain.setAction(action);
+        logDao.addLog(logDomain);
+    }
+
+    @Override
+    public void deleteLogById(Integer id) {
+        if (null == id) {
+            throw new FunctionWallRuntimeException(FunctionWallErrorCode.LOG_ID_IS_NULL);
+        }
+        deleteLogById(id);
+    }
+
+    @Override
+    public PageInfo<LogDomain> getLogs(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<LogDomain> logs = logDao.getLogs();
+        PageInfo<LogDomain> pageInfo = new PageInfo<>(logs);
+        return pageInfo;
+    }
+}
