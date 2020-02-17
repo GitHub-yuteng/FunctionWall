a30506c78f184467fe1936790e352898d2bb11b0
yuteng <soft_yt@163.com>
2020-02-17 19:44:39 +0800

    update:更改MybatisPlusConfig MapperScan BUG：日志 already Create bean Mapper

--- src/main/java/com/functionwall/config/MybatisPlusConfig.java
+++ src/main/java/com/functionwall/config/MybatisPlusConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.functionwall.dao")
 public class MybatisPlusConfig {
 
     /**
